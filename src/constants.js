c78cb88f6b8a66bd2d2fb11a63aba5a158e5ecaf
Noah Sorensen <100672306+noahhts@users.noreply.github.com>
2024-03-19 20:26:12 -0400

    refactor and add transitioning states

--- src/constants.js
+++ src/constants.js
@@ -10,4 +10,4 @@ export const getNextHungerTime = (clock) =>
 export const getNextDieTime = (clock) =>
   Math.floor(Math.random() * 2) + 3 + clock;
 export const getNextPoopTime = (clock) =>
-  Math.floor(Math.random() * 2) + 2 + clock;
+  Math.floor(Math.random() * 3) + 4 + clock;
--- src/gameState.js
+++ src/gameState.js
@@ -17,13 +17,18 @@ const gameState = {
   hungryTime: -1,
   dieTime: -1,
   poopTime: -1,
+  timeToStartCelebrating: -1,
+  timeToEndCelebrating: -1,
   tick() {
     this.clock++;
     console.log("clock", this.clock);
     if (this.clock === this.wakeTime) this.wake();
-    else if (this.clock == this.sleepTime) this.sleep();
-    else if (this.clock == this.hungryTime) this.getHungry();
-    else if (this.clock == this.dieTime) this.die();
+    else if (this.clock === this.sleepTime) this.sleep();
+    else if (this.clock === this.hungryTime) this.getHungry();
+    else if (this.clock === this.dieTime) this.die();
+    else if (this.clock === this.timeToStartCelebrating)
+      this.startCelebrating();
+    else if (this.clock === this.timeToEndCelebrating) this.endCelebrating();
     return this.clock;
   },
   startGame() {
@@ -35,11 +40,11 @@ const gameState = {
   wake() {
     this.current = "IDLING";
     this.wakeTime = -1;
-    modFox("idling");
     this.scene = Math.random() > RAIN_CHANCE ? 0 : 1;
     modScene(SCENES[this.scene]);
     this.sleepTime = this.clock + DAY_LENGTH;
     this.hungryTime = getNextHungerTime(this.clock);
+    this.determineFoxState();
   },
   sleep() {
     this.current = "SLEEP";
@@ -55,6 +60,23 @@ const gameState = {
   },
   die() {
     console.log("die");
+  },
+  startCelebrating() {
+    this.current = "CELEBRATING";
+    modFox("celebrate");
+    this.timeToStartCelebrating = -1;
+    this.timeToEndCelebrating = this.clock + 2;
+  },
+  endCelebrating() {
+    this.current = "IDLING";
+    this.timeToEndCelebrating = -1;
+    this.determineFoxState();
+  },
+  determineFoxState() {
+    if (this.current === "IDLING") {
+      if (SCENES[this.scene] === "rain") modFox("rain");
+      else modFox("idling");
+    }
   },
   handleUserAction(icon) {
     if (
